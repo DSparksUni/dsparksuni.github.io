07ba8f4805eda6e4bca14d57cc4ada80ff502692
KillerD101 <k9d1y0an@gmail.com>
2025-08-02 21:43:47 -0400

    Fixed portfolio to contain links to the github repo

--- js/projects.js
+++ js/projects.js
@@ -6,23 +6,26 @@ const projects = [
     },
 ];
 
-projects.forEach(project => {
-    fetch(`${project.root}/README.md`).then(res => {
-        if(!res.ok) {
-            throw new Error(`HTTP ${res.status}`);
-        }
-        return res.text();
-    }).then(md => {
-        const image_patch = md.replace(
-            /!\[([^\]]*)\]\((?!https?:\/\/)([^)]+)\)/g, 
-            (match, alt, path) => `![${alt}](${project.root}/${path})`
-        );
+function update_projects() {
+    projects.forEach(project => {
+        fetch(`${project.root}/README.md`).then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP ${res.status}`);
+            }
+            return res.text();
+        }).then(md => {
+            const image_patch = md.replace(
+                /!\[([^\]]*)\]\((?!https?:\/\/)([^)]+)\)/g,
+                (_, alt, path) => `![${alt}](${project.root}/${path})`
+            );
 
-        const container = document.getElementById(project.id);
-        container.innerHTML = marked.parse(image_patch);
-    }).catch(err => {
-        const container = document.getElementById(project.id);
-        container.innerHTML =
-            `<p style="color:red;">Failed to load README: ${err.message}</p>`;
+            const container = document.getElementById(project.id);
+            container.innerHTML += marked.parse(image_patch);
+        }).catch(err => {
+            const container = document.getElementById(project.id);
+            container.innerHTML =
+                `<p style="color:red;">Failed to load README: ${err.message}</p>`;
+        });
     });
-});
+}
+update_projects();
